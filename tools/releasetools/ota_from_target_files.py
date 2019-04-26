a43585b662cee4dd62729db5a9a0709510cdf1b6
Davide Garberi <dade.garberi@gmail.com>
2019-04-26 17:34:17 +0200

    build: Allow mounting system properly for A-only system-as-root devices
     * By AOSP build system and recovery design, A-only system-as-root
       image should be mounted at /system_root and /system is a symlink
       to /system_root/system.
     * Still allow mounting system to /system for system-as-root using a variable for devices that eventually need this
    
    Change-Id: I4184c572b25faa8d1990e32f625fa814cba6690f

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -806,6 +806,13 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  is_system_as_root = target_info.get("system_root_image") == "true"
+  if is_system_as_root and not common.system_as_system:
+    system_mount_point = "/system_root"
+  else:
+    system_mount_point = "/system"
+
+  script.AppendExtra("ifelse(is_mounted(\"{0}\"), unmount(\"{0}\"));".format(system_mount_point))
   device_specific.FullOTA_InstallBegin()
 
   system_progress = 0.75
